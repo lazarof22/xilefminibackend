1618dece728f8f55cd5267363519aa3cb34e2f36
Ranta <tandapretelrafael2005@gmail.com>
2026-09-09 16:01:32 -0400

    fix(cliente): PartialType puro y limpia imports sin uso
    UpdateClienteDto vuelve a PartialType puro: quita redeclaraciones
    con @IsNotEmpty() que rompian el PATCH parcial con
    forbidNonWhitelisted. CreateClienteDto: quita imports sin uso
    isNotEmpty/isString.

--- src/modules/clientes y provedores/cliente/dto/update-cliente.dto.ts
+++ src/modules/clientes y provedores/cliente/dto/update-cliente.dto.ts
@@ -1,31 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateClienteDto } from './create-cliente.dto';
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdateClienteDto extends PartialType(CreateClienteDto) {
-    @IsString()
-    @IsNotEmpty()
-    id_cliente: string;
-
-    @IsString()
-    @IsNotEmpty()
-    nombre_cliente: string;
-
-    @IsString()
-    @IsNotEmpty()
-    telefono_cliente: string;
-
-    @IsString()
-    @IsNotEmpty()
-    @IsEmail()
-    email_cliente: string;
-
-    @IsString()
-    @IsNotEmpty()
-    direccion_cliente: string;
-
-    @IsString()
-    @IsNotEmpty()
-    tipo_cliente: string;
-}
-
+export class UpdateClienteDto extends PartialType(CreateClienteDto) {}
